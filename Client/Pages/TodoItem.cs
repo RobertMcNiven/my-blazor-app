dd2b85c937c254fa49b8fa0617379f53c168b563
RobertMcNiven <robertmcnivenjr@fmail.com>
2021-09-29 18:28:25 -0400

    Added todo list to menu and added functionality of todolist

---
+++ Client/Pages/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Client.Pages
+{
+    public class TodoItem
+    /* Creating a class called TodoItem*/
+    {
+        public string Title { get; set; }
+        /* Creating a getter and setter for title field*/
+        public bool IsDone { get; set; }
+        /* Creating getter and setter for IsDone field*/
+    }
+}
