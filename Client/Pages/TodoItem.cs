958d4161d008367f966ca9418c701deaae3dc81f
RobertMcNiven <robertmcnivenjr@fmail.com>
2021-09-29 18:56:23 -0400

    Comments again because oryx

--- Client/Pages/TodoItem.cs
+++ Client/Pages/TodoItem.cs
@@ -6,11 +6,10 @@ using System.Threading.Tasks;
 namespace BlazorApp.Client.Pages
 {
     public class TodoItem
-    /* Creating a class called TodoItem*/
     {
         public string Title { get; set; }
-        /* Creating a getter and setter for title field*/
+        
         public bool IsDone { get; set; }
-        /* Creating getter and setter for IsDone field*/
+        
     }
 }
